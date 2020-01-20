15cbeab07aa8176caef2d9fa68efaccbec58f510
Klara <klara.seitz@googlemail.com>
2020-01-20 17:06:11 +0100

    add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <div id="app">
-    <PatternEditor/>
-  </div>
+  <v-app>
+    <div id="app">
+      <PatternEditor/>
+    </div>
+  </v-app>
 </template>
 
 <script>
--- src/components/ActionToolBar.vue
+++ src/components/ActionToolBar.vue
@@ -1,12 +1,17 @@
+
+
 <template>
     <div>
-        <button text icon color="white">
-            <v-icon>mdi-heart</v-icon>
-        </button>
+        <v-btn class="ma-2" outlined color="indigo">undo</v-btn>
+        <v-btn class="ma-2" outlined color="indigo">redo</v-btn>
+        <v-btn class="ma-2" outlined color="indigo">save</v-btn>
+        <v-btn class="ma-2" outlined color="indigo">open</v-btn>
+        <v-btn class="ma-2" outlined color="indigo">new layer</v-btn>
     </div>
 </template>
 
 <script>
+    import { mdiUndo } from '@mdi/js';
     export default {
         name: "ActionToolBar"
     }
