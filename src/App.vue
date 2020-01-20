aec3ffeb3a387b0b7f045455a458d3f25e10a7bf
Klara <klara.seitz@googlemail.com>
2020-01-20 14:50:16 +0100

    initial setup with force graph canvas

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <GraphCanvas/>
+  </div>
+</template>
+
+<script>
+import GraphCanvas from "@/components/GraphCanvas";
+
+export default {
+  name: 'app',
+  components: {
+    GraphCanvas
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/GraphCanvas.vue
@@ -0,0 +1,44 @@
+<template>
+    <div ref="canvas"></div>
+</template>
+
+<script >
+    import ForceGraph from 'force-graph'
+
+    const N = 300;
+    const gData = {
+        nodes: [...Array(N).keys()].map(i => ({ id: i })),
+        links: [...Array(N).keys()]
+            .filter(id => id)
+            .map(id => ({
+                source: id,
+                target: Math.round(Math.random() * (id-1))
+            }))
+    };
+
+
+
+
+
+    export default {
+        data() {
+            return {
+                name: 'GraphCanvas'
+            }
+        },
+
+        mounted() {
+            console.log('mounted Hook');
+            const Graph = ForceGraph();
+            let element = this.$refs.canvas;
+            /* eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
+            console.log(element);
+            Graph(element)
+                .graphData(gData);
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
